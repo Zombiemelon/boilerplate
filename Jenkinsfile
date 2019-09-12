4986da7367e6895bf1259b7337633837797b1d09
svetoslavdimitrov <eat@me.com>
2019-09-12 08:51:44 +0300

    Full deploy

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,27 +6,27 @@ pipeline {
         ECR_ADDRESS='276242186269.dkr.ecr.eu-central-1.amazonaws.com/inex'
     }
     stages {
-//         stage ('Build Back') {
-//             steps {
-//                 sh 'ls -alh'
-//                 sh 'docker build -t $CONTAINER_NAME:back -f ./docker/Dockerfile.staging.backend .'
-//                 sh '$(/root/.local/bin/aws ecr get-login --no-include-email --region eu-central-1)'
-//                 sh 'docker tag $CONTAINER_NAME:back $ECR_ADDRESS:back'
-//                 sh 'docker push $ECR_ADDRESS'
-//                 sh 'echo "Delete image"'
-//                 sh 'docker image rm -f ${CONTAINER_NAME}:back && docker image prune -f'
-//             }
-//         }
-//         stage ('Build Front') {
-//             steps {
-//                 sh 'docker build -t $CONTAINER_NAME:front -f ./docker/Dockerfile.staging.frontend . '
-//                 sh '$(/root/.local/bin/aws ecr get-login --no-include-email --region eu-central-1)'
-//                 sh 'docker tag $CONTAINER_NAME:front $ECR_ADDRESS:front'
-//                 sh 'docker push $ECR_ADDRESS:front'
-//                 sh 'echo "Delete image"'
-//                 sh 'docker image rm -f ${CONTAINER_NAME}:front && docker image prune -f'
-//             }
-//         }
+        stage ('Build Back') {
+            steps {
+                sh 'ls -alh'
+                sh 'docker build -t $CONTAINER_NAME:back -f ./docker/Dockerfile.staging.backend .'
+                sh '$(/root/.local/bin/aws ecr get-login --no-include-email --region eu-central-1)'
+                sh 'docker tag $CONTAINER_NAME:back $ECR_ADDRESS:back'
+                sh 'docker push $ECR_ADDRESS'
+                sh 'echo "Delete image"'
+                sh 'docker image rm -f ${CONTAINER_NAME}:back && docker image prune -f'
+            }
+        }
+        stage ('Build Front') {
+            steps {
+                sh 'docker build -t $CONTAINER_NAME:front -f ./docker/Dockerfile.staging.frontend . '
+                sh '$(/root/.local/bin/aws ecr get-login --no-include-email --region eu-central-1)'
+                sh 'docker tag $CONTAINER_NAME:front $ECR_ADDRESS:front'
+                sh 'docker push $ECR_ADDRESS:front'
+                sh 'echo "Delete image"'
+                sh 'docker image rm -f ${CONTAINER_NAME}:front && docker image prune -f'
+            }
+        }
         stage('Deploy') {
             steps {
                 sshPublisher(publishers: [sshPublisherDesc(configName: 'deploy', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: "\$(aws ecr get-login --no-include-email --region eu-central-1) && docker pull ${ECR_ADDRESS}:back && docker pull ${ECR_ADDRESS}:front && docker rm -f delta_dashboard_backend && docker rm -f delta_dashboard_frontend ; docker run --name delta_dashboard_backend -d -p 8002:80 ${ECR_ADDRESS}:back && docker run --name delta_dashboard_frontend -d -p 4000:80 ${ECR_ADDRESS}:front", execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
--- inex_frontend/src/Containers/Invoice.jsx
+++ inex_frontend/src/Containers/Invoice.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect }  from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 import axios from "../Components/Axios/Axios";
 
 const useStyles = makeStyles(theme => ({
@@ -33,72 +34,64 @@ export default function invoice () {
 
 
     return (
-        <form action="http://localhost:8001/api/testpdf" method="get" noValidate autoComplete="off">
-            <div className={classes.container}>
-                <TextField
-                    id="outlined-name"
-                    label="Number"
-                    name="number"
-                    defaultValue="97"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-name"
-                    label="Date"
-                    name="date"
-                    tyoe="date"
-                    defaultValue="22-08-2019"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-name"
-                    label="Date of loading"
-                    name="date_of_loading"
-                    defaultValue="23-24/08/2019"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-name"
-                    label="Truck Number"
-                    name="truck_number"
-                    defaultValue="NT-30-BKM / NT-31-BKM"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-name"
-                    label="Driver Name"
-                    name="driver_name"
-                    defaultValue="Чиореску Андрей (Ciorescu Andrei)"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-name"
-                    label="Driver Passport"
-                    name="driver_passport"
-                    defaultValue="АВ 0520302"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                    type="submit">
-                    Download invoice
-                </Button>
-            </div>
-        </form>
+        <Container maxWidth="sm">
+            <form action="http://localhost:8001/api/testpdf" method="get" noValidate autoComplete="off">
+                <div className={classes.container}>
+                    <TextField
+                        id="outlined-name"
+                        label="Number"
+                        name="number"
+                        defaultValue="97"
+                        className={classes.textField}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-name"
+                        label="Date of loading"
+                        name="date_of_loading"
+                        defaultValue="23-24/08/2019"
+                        className={classes.textField}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-name"
+                        label="Truck Number"
+                        name="truck_number"
+                        defaultValue="NT-30-BKM / NT-31-BKM"
+                        className={classes.textField}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-name"
+                        label="Driver Name"
+                        name="driver_name"
+                        defaultValue="Чиореску Андрей (Ciorescu Andrei)"
+                        className={classes.textField}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-name"
+                        label="Driver Passport"
+                        name="driver_passport"
+                        defaultValue="АВ 0520302"
+                        className={classes.textField}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        type="submit">
+                        Download invoice
+                    </Button>
+                </div>
+            </form>
+        </Container>
     )
 };
 
