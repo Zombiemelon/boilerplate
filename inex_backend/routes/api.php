19967188e68273ba215c719de9659bf946fb5a00
svetoslavdimitrov <eat@me.com>
2019-09-22 23:37:24 +0300

    Added Document Middleware

--- inex_backend/routes/api.php
+++ inex_backend/routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:api')->get('/user', function(Request $request) {
     return $request->user();
 });
 Route::post('/login', 'Auth\LoginController@login');
-Route::get('/document', 'DocumentController@getDocument');
+Route::get('/document', 'DocumentController@getDocument')->middleware('document');
 Route::get('/drivers', 'DocumentController@getAllDrivers');
 Route::get('/trucks', 'DocumentController@getAllCars');
 Route::get('/last_document_number', 'DocumentController@getLastDocumentNumber');
