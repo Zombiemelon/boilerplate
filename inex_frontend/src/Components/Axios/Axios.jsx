221c6ccae3b1c6eaea3279b61754cd5c655b8fd1
svetoslavdimitrov <eat@me.com>
2019-10-05 07:46:48 +0300

    Try CI with Codeception network change

--- inex_frontend/src/Components/Axios/Axios.jsx
+++ inex_frontend/src/Components/Axios/Axios.jsx
@@ -3,7 +3,7 @@ const axios = require('axios');
 const apiToken = JSON.parse(localStorage.getItem('api_token'));
 
 const instance = axios.create({
-    baseURL: `http://${window.location.hostname}:8001`,
+    baseURL: `inex_back:8001`,
     timeout: 50000,
     headers: {
         'Access-Control-Allow-Origin': '*',
